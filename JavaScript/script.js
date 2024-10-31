1112ef23e1427561ddc44ba8699905153ec0d2ca
ajdeleon661 <ajdeleon661@gmail.com>
2024-10-30 20:00:57 -0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,1 @@
-let map;
-let startingPosition = { lat: 34.0549, lng: -118.2426};
 
-async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-
-    map = new Map(document.getElementById("map"), {
-        center: startingPosition,
-        zoom: 12,
-    });
-}
-initMap();
